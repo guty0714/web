e7e3874f1c6bb90273146a926a1c76c0b7f79ae1
JpGuty <juan_p.gutierrez@uao.edu.co>
2025-11-11 02:53:08 -0500

    Proyecto web

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => { res.sendFile(path.join(__dirname, "public", "index.html")); });
+app.listen(5000, () => { console.log("Servidor iniciado en el puerto 5000"); });
